939e57a1c43110793ba3aaf35d754503665984a8
Karel <ke4@sanger.ac.uk>
2016-10-12 09:26:10 +0100

    Revert back the config values in the test

--- src/test/groovy/uk/ac/sanger/scgcf/jira/lims/configurations/ConfigReaderTest.groovy
+++ src/test/groovy/uk/ac/sanger/scgcf/jira/lims/configurations/ConfigReaderTest.groovy
@@ -7,11 +7,11 @@ class ConfigReaderTest extends Specification {
     def 'test can read config file'() {
 
         expect: "test can read custom field aliases"
-        assert ConfigReader.getCFName("UAT_CUST_TUBE_BARCODES") == "Tiny Created Tubes Barcode field"
+        assert ConfigReader.getCFName("UAT_CUST_TUBE_BARCODES") == "UAT cust tube barcodes"
         assert ConfigReader.getCFId("UAT_CUST_TUBE_BARCODES") == 10900
         assert ConfigReader.getCFIdString("UAT_CUST_TUBE_BARCODES") == "customfield_10900"
 
-        assert ConfigReader.getCFName("UAT_CUST_TUBE_DETAILS") == "Tiny Customer Tube Details"
+        assert ConfigReader.getCFName("UAT_CUST_TUBE_DETAILS") == "UAT cust tube details"
         assert ConfigReader.getCFId("UAT_CUST_TUBE_DETAILS") == 10901
         assert ConfigReader.getCFIdString("UAT_CUST_TUBE_DETAILS") == "customfield_10901"
 
